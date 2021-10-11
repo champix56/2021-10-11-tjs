f252b939659ca66c74f56818b7b79c7c838c8731
champix <desorbaix@free.fr>
2021-10-11 16:14:43 +0200

    extend by className

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/finger-256.png" />
         Cliquez ici
       </Button>
-      <Button text="Un boutton" lorsqueJeClique={()=>{}}/>
+      <Button text="Un boutton" className="warning" lorsqueJeClique={()=>{}}/>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types'
 import styles from "./Button.module.css";
 const Button = (props) => {
   console.log(props);
-  return (
+  var classValue=props.className && props.className.includes('warning')?styles.Button+' '+styles.warning:styles.Button;
+    return (
     <button
       type={props.type}
-      className={styles.Button}
+      className={classValue}
       onClick={(evt) => {
         console.log(evt);
         props.lorsqueJeClique("C'est bon j'ai ete cliqué, parent je t'informe !!! ");
@@ -20,5 +21,6 @@ Button.propTypes = {
     lorsqueJeClique: PropTypes.func.isRequired,
     text: PropTypes.string,
     children: PropTypes.any,
+    className: PropTypes.string,
 };
 export default Button;
