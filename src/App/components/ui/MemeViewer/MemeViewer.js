15f9e768befc196089840fb95644d4ec84c50e93
champix <desorbaix@free.fr>
2021-10-12 15:07:44 +0200

    MEO du viewer

--- src/App/components/ui/MemeViewer/MemeViewer.js
+++ src/App/components/ui/MemeViewer/MemeViewer.js
@@ -1,10 +1,47 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./MemeViewer.module.css";
-
+export const demoMeme = {
+  meme: {
+    id: 0,
+    imageId: 0,
+    text: "j'ai dit les function c'est mieux que les class",
+    x: 10,
+    y: 200,
+    fontSize: 50,
+    fontWeight: "900",
+    color: "BLUE",
+    underline: true,
+    italic: true,
+  },
+  image: {
+    url: "/img/memes/1ermeme.jpg",
+    titre: "1er meme",
+    id: 0,
+    w: 1000,
+    h: 938,
+  },
+};
 const MemeViewer = (props) => (
-  <svg className={styles.MemeViewer} data-testid="MemeViewer">
-    MemeViewer Component
+  <svg
+    className={styles.MemeViewer}
+    data-testid="MemeViewer"
+    viewBox={`0 0 ${props.image.w} ${props.image.h}`}
+  >
+    <image href={props.image.url} />
+    <text
+      x={props.meme.x}
+      y={props.meme.y}
+      fill={props.meme.color}
+      style={{
+        fontWeight: props.meme.fontWeight,
+        fontSize: props.meme.fontSize,
+        textDecoration: props.meme.underline ? "underline" : "none",
+        fontStyle: props.meme.italic ? "italic" : "normal",
+      }}
+    >
+      {props.meme.text}
+    </text>
   </svg>
 );
 
@@ -13,26 +50,6 @@ MemeViewer.propTypes = {
   image: PropTypes.object,
 };
 
-MemeViewer.defaultProps = {
-  meme: {
-    id: 0,
-    imageId: 0,
-    text: 'Mon 1er meme',
-    x: 10,
-    y: 10,
-    fontSize: 10,
-    fontWeight: '900',
-    color: 'BLUE',
-    underline: true,
-    italic: false,
-  },
-  image: {
-    url: '/img/memes/1ermeme.jpg',
-    titre:'1er meme',
-    id: 0,
-    w: 1000,
-    h: 938,
-  },
-};
+MemeViewer.defaultProps = {};
 
 export default MemeViewer;
