b1ca8fe7d0f2af38700147a0b8b247fcb315cc04
champix <desorbaix@free.fr>
2021-10-12 09:19:32 +0200

    eesaie d'une lib lambda

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button/Button'
+import  {CloudPoints, Histogram as Histo} from 'react-graphical-ui'
 function App() {
+  let mesVaules=[12,58,45,65,84,123,151,12,0,45]
   return (
     <div className="App">
+      <Histo values={mesVaules} display={'inline'} max={152} />
+      <CloudPoints values={mesVaules} display={'inline'} max={152} />
+
       <Button lorsqueJeClique={ (argument1)=> {alert('J\'ai cliqué sur Benjamin');console.log(argument1);}} >
         <img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/finger-256.png" />
         Cliquez ici
