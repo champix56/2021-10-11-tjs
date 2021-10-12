183f48240f9a050bcd83b90b1771c34892d21176
champix <desorbaix@free.fr>
2021-10-12 14:32:09 +0200

    definition de la forme d'un meme de props memeViewer

--- src/App/components/ui/MemeViewer/MemeViewer.js
+++ src/App/components/ui/MemeViewer/MemeViewer.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './MemeViewer.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./MemeViewer.module.css";
 
-const MemeViewer = () => (
-  <div className={styles.MemeViewer} data-testid="MemeViewer">
+const MemeViewer = (props) => (
+  <svg className={styles.MemeViewer} data-testid="MemeViewer">
     MemeViewer Component
-  </div>
+  </svg>
 );
 
-MemeViewer.propTypes = {};
+MemeViewer.propTypes = {
+  meme: PropTypes.object.isRequired,
+  image: PropTypes.object,
+};
 
-MemeViewer.defaultProps = {};
+MemeViewer.defaultProps = {
+  meme: {
+    id: 0,
+    imageId: 0,
+    text: 'Mon 1er meme',
+    x: 10,
+    y: 10,
+    fontSize: 10,
+    fontWeight: '900',
+    color: 'BLUE',
+    underline: true,
+    italic: false,
+  },
+  image: {
+    url: '/img/memes/1ermeme.jpg',
+    titre:'1er meme',
+    id: 0,
+    w: 1000,
+    h: 938,
+  },
+};
 
 export default MemeViewer;
