6f4f2fa6b03de043a93f09b07497697dffeb8ab4
champix <desorbaix@free.fr>
2021-10-12 16:49:11 +0200

    mise en place d'un fetch sur cmp

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,25 @@ import Navbar from './components/ui/Navbar/Navbar';
 import FlexLayout from './components/layouts/FlexLayout/FlexLayout';
 import MemeViewer, { demoMeme as defaultmeme } from './components/ui/MemeViewer/MemeViewer';
 import MemeForm from './components/functionnals/MemeForm/MemeForm';
+import { ADR_SRV, RESSOURCES_NAME } from './config/config';
 interface Props {
 
 }
 interface State {
-  counter: number
+  memes:Array<{}>,
+  images:Array<{}>
 }
 
 export default class App extends Component<Props, State> {
-  state = { counter: 0, champs2: 0 }
+  state = { memes:[],images:[] }
   componentDidMount() {
-
+    const pmemes=fetch(`${ADR_SRV}${RESSOURCES_NAME.memes}`).then(f=>f.json())
+    const pimages=fetch(`${ADR_SRV}${RESSOURCES_NAME.images}`).then(f=>f.json())
+    Promise.all([pmemes,pimages])
+      .then(ar_ar=>{
+        this.setState({memes:ar_ar[0], images:ar_ar[1]});
+        return ar_ar;
+      })
   }
   componentDidUpdate() {
     console.log(arguments);
@@ -30,6 +38,7 @@ export default class App extends Component<Props, State> {
             <MemeViewer meme={defaultmeme.meme} image={defaultmeme.image} />
             <MemeForm />
           </FlexLayout>
+
         </div>
       </>
     )
