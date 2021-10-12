c824735140fd1a6e33b380212b05009bdc23666a
champix <desorbaix@free.fr>
2021-10-12 15:06:59 +0200

    MEO du viewer de meme

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/ui/Navbar/Navbar';
 import FlexLayout from './components/layouts/FlexLayout/FlexLayout';
-import MemeViewer from './components/ui/MemeViewer/MemeViewer';
+import MemeViewer, {demoMeme as defaultmeme} from './components/ui/MemeViewer/MemeViewer';
 import MemeForm from './components/functionnals/MemeForm/MemeForm';
 interface Props {
 
@@ -25,7 +25,7 @@ export default class App extends Component<Props, State> {
       <div className="App">
         <Navbar></Navbar>
         <FlexLayout>
-          <MemeViewer />
+          <MemeViewer meme={defaultmeme.meme} image={defaultmeme.image} />
           <MemeForm />
         </FlexLayout>
       </div>
