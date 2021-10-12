3b91f49eb62aa8f469c76d4b19e5937b3fc4563a
champix <desorbaix@free.fr>
2021-10-12 11:04:21 +0200

    mise en place d'etats avec class et useState +cycle de vie class et useEffect

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 import Button from './components/Button/Button'
-import  {CloudPoints, Histogram as Histo} from 'react-graphical-ui'
-function App() {
-  let mesVaules=[12,58,45,65,84,123,151,12,0,45]
-  return (
-    <div className="App">
-      <Histo values={mesVaules} display={'inline'} max={152} />
-      <CloudPoints values={mesVaules} display={'inline'} max={152} />
 
-      <Button lorsqueJeClique={ (argument1)=> {alert('J\'ai cliqué sur Benjamin');console.log(argument1);}} >
-        <img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/finger-256.png" />
-        Cliquez ici
-      </Button>
-      <Button text="Un boutton" color="blue" style={{textDecoration:'underline'}}/>
-    </div>
-  );
+interface Props {
+  
+}
+interface State {
+  counter:number
 }
 
-export default App;
+export default class App extends Component<Props, State> {
+  state = {counter:0, champs2:0}
+  componentDidMount(){
+    
+  }
+  componentDidUpdate(){
+    console.log(arguments);
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div>
+        valeur du compteur :{this.state.counter}
+        <Button lorsqueJeClique={()=>{
+            this.setState({counter:this.state.counter+1});
+           // console.log(this.state);
+        }}>Cliquez ici</Button>
+      </div>
+    )
+  }
+}
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,34 +1,51 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 const Button = (props) => {
-  console.log(props);
-    return (
+  const [isClicked, setisClicked] = useState(false);
+
+  useEffect(() => {
+   console.log('%c%s','color:red;font-size:x-large', 'changement d\'etat cliqué du button')
+  }, [isClicked])
+
+  return (
     <button
       type={props.type}
-      className={`${styles.Button} ${styles.clicked}`}
-      style={{...props.style,backgroundColor:props.bgColor,color:props.color}}
+      className={`${styles.Button} ${isClicked ? " " + styles.clicked : ""}`}
+      style={{
+        ...props.style,
+        backgroundColor: props.bgColor,
+        color: props.color,
+      }}
       onClick={(evt) => {
-        console.log(evt);
-        props.lorsqueJeClique("C'est bon j'ai ete cliqué, parent je t'informe !!! ");
+        //console.log(evt);
+        setisClicked(true);
+        setTimeout(() => {
+          setisClicked(false);
+        }, 200);
+
+        props.lorsqueJeClique(
+          "C'est bon j'ai ete cliqué, parent je t'informe !!! "
+        );
       }}
     >
       {undefined !== props.children ? props.children : props.text}
+      {/* <br/>{isClicked?'cliqué':'pas cliqué'} */}
     </button>
   );
 };
 
 Button.propTypes = {
-    lorsqueJeClique: PropTypes.func.isRequired,
-    text: PropTypes.string,
-    children: PropTypes.any,
-    bgColor: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    style: PropTypes.object,
+  lorsqueJeClique: PropTypes.func.isRequired,
+  text: PropTypes.string,
+  children: PropTypes.any,
+  bgColor: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  style: PropTypes.object,
 };
-Button.defaultProps={
-    bgColor: 'skyblue',
-    color: 'white',
-    lorsqueJeClique:()=>{}
-}
+Button.defaultProps = {
+  bgColor: "skyblue",
+  color: "white",
+  lorsqueJeClique: () => {},
+};
 export default Button;
