465552c9a8d5951ad3b248eff43dfaac8ab0bb1d
champix <desorbaix@free.fr>
2021-10-13 08:30:14 +0200

    creation Thumb mis connexion etat

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,25 +2,46 @@ import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/ui/Navbar/Navbar';
 import FlexLayout from './components/layouts/FlexLayout/FlexLayout';
-import MemeViewer, { demoMeme as defaultmeme } from './components/ui/MemeViewer/MemeViewer';
+import MemeViewer, { demoMeme as defaultmeme, demoMeme } from './components/ui/MemeViewer/MemeViewer';
 import MemeForm from './components/functionnals/MemeForm/MemeForm';
 import { ADR_SRV, RESSOURCES_NAME } from './config/config';
+import MemeThumbnail from './components/layouts/MemeThumbnail/MemeThumbnail';
 interface Props {
 
 }
 interface State {
-  memes:Array<{}>,
-  images:Array<{}>
+  memes: Array<{}>,
+  images: Array<{}>,
+  currentMeme:Meme
+}
+interface Meme {
+  id?: number,
+  imageId: number,
+  text: string
+  x: number,
+  y: number,
+  fontSize: number,
+  fontWeight: string,
+  color: string,
+  underline: boolean,
+  italic: boolean,
+};
+interface Image {
+  url: string,
+  titre: string,
+  id: number,
+  w: number,
+  h: number,
 }
 
 export default class App extends Component<Props, State> {
-  state = { memes:[],images:[] }
+  state = { memes: [], images: [], currentMeme:demoMeme.meme}
   componentDidMount() {
-    const pmemes=fetch(`${ADR_SRV}${RESSOURCES_NAME.memes}`).then(f=>f.json())
-    const pimages=fetch(`${ADR_SRV}${RESSOURCES_NAME.images}`).then(f=>f.json())
-    Promise.all([pmemes,pimages])
-      .then(ar_ar=>{
-        this.setState({memes:ar_ar[0], images:ar_ar[1]});
+    const pmemes = fetch(`${ADR_SRV}${RESSOURCES_NAME.memes}`).then(f => f.json())
+    const pimages = fetch(`${ADR_SRV}${RESSOURCES_NAME.images}`).then(f => f.json())
+    Promise.all([pmemes, pimages])
+      .then(ar_ar => {
+        this.setState({ memes: ar_ar[0], images: ar_ar[1] });
         return ar_ar;
       })
   }
@@ -34,9 +55,17 @@ export default class App extends Component<Props, State> {
         <div>{JSON.stringify(this.state)}</div>
         <div className="App">
           <Navbar></Navbar>
+          <MemeThumbnail>
+            {
+              this.state.memes.map((e:Meme, i:number) =><MemeViewer meme={e} image={this.state.images.find((ee:Image) => ee.id === e.imageId)} />)
+
+            }
+          </MemeThumbnail>
+
+
           <FlexLayout>
-            <MemeViewer meme={defaultmeme.meme} image={defaultmeme.image} />
-            <MemeForm />
+            <MemeViewer meme={this.state.currentMeme} image={this.state.images.find((e:Image)=>e.id===this.state.currentMeme.imageId)} />
+            <MemeForm meme={this.state.currentMeme} onFormChange={(meme:Meme)=>{this.setState({currentMeme:meme})}} />
           </FlexLayout>
 
         </div>
--- src/App/components/functionnals/MemeForm/MemeForm.js
+++ src/App/components/functionnals/MemeForm/MemeForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './MemeForm.module.css'
+import Button from '../../ui/Button/Button';
 const initialState={}
 function MemeForm(props) {
     const [state, setstate] = useState(initialState);
@@ -11,12 +12,15 @@ function MemeForm(props) {
     }, [state])
     return (
         <div className={styles.MemeForm} data-testid="MemeForm">
-            memeForm state -&gt;  {JSON.stringify(state)}
+            memeForm state -&gt;  {JSON.stringify(props.meme)}
+            <Button lorsqueJeClique={(evt)=>{props.onFormChange({...props.meme,text:'C\'est la fin de la journee'})}}>Changer le text</Button>
         </div>
     )
 }
 
 MemeForm.propTypes = {
+  meme: PropTypes.object.isRequired,
+  onFormChange: PropTypes.func.isRequired,
 }
 
 export default MemeForm
