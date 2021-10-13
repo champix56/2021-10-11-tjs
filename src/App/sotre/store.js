c504d0cc5c992e6d3c5a877b3b281ee855adcd9e
champix <desorbaix@free.fr>
2021-10-13 11:52:17 +0200

    enum + export

--- src/App/sotre/store.js
+++ src/App/sotre/store.js
@@ -1,12 +1,19 @@
-const initialState = {
+export const initialState = {
     memes:[],
     images:[]
 }
-
+export const REDUCER_ACTIONS=Object.freeze({
+    INIT_IMAGES:'INIT_IMAGES',
+    ADD_MEME:'ADD_MEME'
+})
+const REDUCER_PRIVATE_ACTIONS=Object.freeze({
+    FULL_INIT:'FULL_INIT',
+    FETCH_ALL:'FETCH_ALL'
+})
 function reducer (state = initialState, action) {
     switch (action.type) {
-        case 'INIT_IMAGES' :return {...state,images:[...action.values]};
-        case 'ADD_MEME' :return {...state,memes:[ ...state.memes, action.value ] };
+        case REDUCER_ACTIONS.INIT_IMAGES :return {...state,images:[...action.values]};
+        case  REDUCER_ACTIONS.ADD_MEME :return {...state,memes:[ ...state.memes, action.value ] };
         default:
             return state;
     }
@@ -14,8 +21,8 @@ function reducer (state = initialState, action) {
 let state=reducer(undefined,{type:'INIT'});
 console.log(state);
 
-state=reducer(state,{type:'INIT_IMAGES', values:[{id:0},{id:1},{id:2}]});
+state=reducer(state,{type:REDUCER_ACTIONS.INIT_IMAGES, values:[{id:0},{id:1},{id:2}]});
 console.log(state);
 
-state=reducer(state,{type:'ADD_MEME', value:{id:0,text:'coucou'}});
+state=reducer(state,{type:REDUCER_ACTIONS.ADD_MEME, value:{id:0,text:'coucou'}});
 console.log(state);
