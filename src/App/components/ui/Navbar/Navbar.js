616a84059e565a3866c76f3b1f5cbb7f820328c9
champix <desorbaix@free.fr>
2021-10-13 10:44:49 +0200

    Mise en place d'une navbar rbs + linkContainer rrb

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Navbar.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
+import { Navbar as Nb, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+import "../../../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import { LinkContainer } from "react-router-bootstrap";
 const Navbar = () => (
   <div className={styles.Navbar} data-testid="Navbar">
-    
+    <Nb bg="primary" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Nb.Brand href="#home">
+            <span className={styles.title}>
+              <span className={styles.titlememe}>Meme</span>
+              <span className={styles.titlecoma}>.</span>
+              <span className={styles.titlejs}>js</span>
+            </span>
+          </Nb.Brand>
+        </LinkContainer>
+        <Nav className="me-auto">
+          <LinkContainer to="/Thumbnail">
+            <Nav.Link href="#">Thumbnail</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/Editor">
+            <Nav.Link href="#">Editor</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/Editor/0">
+            <Nav.Link href="#pricing">Editor/0</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Container>
+    </Nb>
   </div>
 );
 
